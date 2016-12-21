01cc8c14841b55a926218e02f3cbdd5c26c7de2f
Malte Hübner <maltehuebner@gmx.org>
2016-12-21 20:14:51 +0100

    Add Paragraph entity.

---
+++ src/AppBundle/Entity/Paragraph.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Caldera\Bundle\StvoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="paragraph")
+ */
+class Paragraph
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $number;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $text;
+
+    public function __construct()
+    {
+
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setNumber(string $number): Paragraph
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getNumber(): ?string
+    {
+        return $this->number;
+    }
+
+    public function setTitle(string $title): Paragraph
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setText(string $text): Paragraph
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+}
