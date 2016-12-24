aa59f1e79e44ca1aa4cc3f533574f22b20aa1d81
Malte Hübner <maltehuebner@gmx.org>
2016-12-24 03:08:01 +0100

    WIP at importing paragraphs.

--- src/Caldera/Bundle/StvoBundle/Command/LoadLawCommand.php
+++ src/Caldera/Bundle/StvoBundle/Command/LoadLawCommand.php
@@ -2,9 +2,17 @@
 
 namespace Caldera\Bundle\StvoBundle\Command;
 
+use Caldera\Bundle\StvoBundle\Entity\Law;
+use Caldera\Bundle\StvoBundle\Entity\Paragraph;
+use Caldera\Bundle\StvoBundle\Entity\Version;
+use DirectoryIterator;
 use Doctrine\ORM\EntityManager;
+use PHPGit\Git;
+use SplFileInfo;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class LoadLawCommand extends ContainerAwareCommand
@@ -12,13 +20,95 @@ class LoadLawCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            ->setName('stvo:load:law')
-            ->setDescription('Load law');
+            ->setName('stvo:law:load')
+            ->setDescription('Load law')
+            ->addArgument(
+                'law',
+                InputArgument::REQUIRED
+            )
+            ->addArgument(
+                'version',
+                InputArgument::REQUIRED
+            )
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $lawSlug = $input->getArgument('law');
+        $versionSlug = $input->getArgument('version');
+
+        $varDirectory = $this->getContainer()->getParameter('kernel.root_dir') . '../var/tmp/';
+        $gitDirectory = $varDirectory . 'law/' . $lawSlug . '/' . $versionSlug;
+
+        /* @TODO Git stuff goes here */
+
         /** @var EntityManager $entityManager */
         $entityManager = $this->getContainer()->get('doctrine')->getManager();
+
+        $law = $entityManager->getRepository('CalderaStvoBundle:Law')->findOneBySlug($lawSlug);
+        $version = $entityManager->getRepository('CalderaStvoBundle:Version')->findOneBySlug($versionSlug);
+
+        $this->createOrUpdateParagraphs($input, $output, $gitDirectory, $law, $version);
+
+        $entityManager->flush();
+    }
+
+    protected function createOrUpdateParagraphs(
+        InputInterface $input,
+        OutputInterface $output,
+        string $directory,
+        Law $law,
+        Version $version
+    )
+    {
+        $dir = new DirectoryIterator($directory);
+
+        foreach ($dir as $fileinfo) {
+            if ($fileinfo->isFile()) {
+                $this->createOrUpdateParagraph($input, $output, $fileinfo, $law, $version);
+            }
+        }
+    }
+
+    protected function createOrUpdateParagraph(
+        InputInterface $input,
+        OutputInterface $output,
+        SplFileInfo $fileInfo,
+        Law $law,
+        Version $version
+    )
+    {
+        $filename = $fileInfo->getFilename();
+
+        preg_match('/(.{1,3}) (.*).html/', $filename, $matches);
+
+        $number = $matches[1];
+        $title = $matches[2];
+        $text = file_get_contents($fileInfo->getPathname());
+
+        /** @var EntityManager $entityManager */
+        $entityManager = $this->getContainer()->get('doctrine')->getManager();
+
+        $paragraph = $entityManager->getRepository('CalderaStvoBundle:Paragraph')->findOneByLawVersionNumber(
+            $law,
+            $version,
+            $number
+        );
+
+        if (!$paragraph) {
+            $paragraph = new Paragraph();
+
+            $paragraph->setNumber($number);
+        }
+
+        $paragraph
+            ->setTitle($title)
+            ->setText($text)
+            ->setLaw($law)
+            ->setVersion($version)
+        ;
+
+        $entityManager->persist($paragraph);
     }
 }
--- src/Caldera/Bundle/StvoBundle/Repository/ParagraphRepository.php
+++ src/Caldera/Bundle/StvoBundle/Repository/ParagraphRepository.php
@@ -4,12 +4,23 @@ namespace Caldera\Bundle\StvoBundle\Repository;
 
 use Caldera\Bundle\StvoBundle\Entity\Law;
 use Caldera\Bundle\StvoBundle\Entity\Paragraph;
+use Caldera\Bundle\StvoBundle\Entity\Version;
 use Doctrine\ORM\EntityRepository;
 
 class ParagraphRepository extends EntityRepository
 {
-    public function getOneByLawVersion(Law $law, $version): ?Paragraph
+    public function findOneByLawVersionNumber(Law $law, Version $version, string $number): ?Paragraph
     {
+        $qb = $this->createQueryBuilder('p');
 
+        $qb
+            ->where($qb->expr()->eq('p.law', $law->getId()))
+            ->andWhere($qb->expr()->eq('p.version', $version->getId()))
+            ->andWhere($qb->expr()->eq('p.number', '\'' . $number . '\''))
+        ;
+
+        $query = $qb->getQuery();
+
+        return $query->getOneOrNullResult();
     }
 }
