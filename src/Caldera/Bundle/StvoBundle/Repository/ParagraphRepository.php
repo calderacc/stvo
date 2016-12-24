be9ca9f8d805491aca9e602ddc1599d9478b3de6
Malte Hübner <maltehuebner@gmx.org>
2016-12-24 03:15:37 +0100

    Add findByLawVersion() method to ParagraphRepository.

--- src/Caldera/Bundle/StvoBundle/Repository/ParagraphRepository.php
+++ src/Caldera/Bundle/StvoBundle/Repository/ParagraphRepository.php
@@ -9,6 +9,22 @@ use Doctrine\ORM\EntityRepository;
 
 class ParagraphRepository extends EntityRepository
 {
+    public function findByLawVersion(Law $law, Version $version): array
+    {
+        $qb = $this->createQueryBuilder('p');
+
+        $qb
+            ->where($qb->expr()->eq('p.law', $law->getId()))
+            ->andWhere($qb->expr()->eq('p.version', $version->getId()))
+        ;
+
+        $qb->orderBy('p.number', 'ASC');
+
+        $query = $qb->getQuery();
+
+        return $query->getResult();
+    }
+
     public function findOneByLawVersionNumber(Law $law, Version $version, string $number): ?Paragraph
     {
         $qb = $this->createQueryBuilder('p');
